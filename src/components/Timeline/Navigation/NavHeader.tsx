46caf61c3c120e534ac887370beef3de838df94a
Deka <dekachan16@gmail.com>
2020-06-20 11:27:13 +0900

    feat: Impl navigation header

---
+++ src/components/Timeline/Navigation/NavHeader.tsx
@@ -0,0 +1,6 @@
+import React, { FC } from 'react'
+import styles from './NavHeader.module.scss'
+
+export const NavHeader: FC = () => {
+  return <div className={styles.container}>タイムライン</div>
+}
--- src/components/Timeline/Navigation/index.tsx
+++ src/components/Timeline/Navigation/index.tsx
@@ -1,6 +1,11 @@
 import React, { FC } from 'react'
 import styles from './index.module.scss'
+import { NavHeader } from './NavHeader'
 
 export const Navigation: FC = () => {
-  return <div className={styles.container}></div>
+  return (
+    <div className={styles.container}>
+      <NavHeader />
+    </div>
+  )
 }
