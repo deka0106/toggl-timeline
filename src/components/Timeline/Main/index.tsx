b6952183156e28bdd6a179b95bd0d5e607ce14f8
Deka <dekachan16@gmail.com>
2020-11-21 17:03:11 +0900

    feat: Impl resizable height of time row

--- src/components/Timeline/Main/index.tsx
+++ src/components/Timeline/Main/index.tsx
@@ -1,15 +1,31 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect, useRef } from 'react'
 import styles from './index.module.scss'
 import { TimelineHeader } from './TimelineHeader'
 import { TimelineContent } from './TimelineContent'
+import { useStore } from 'store'
 
 export const Main: FC = () => {
+  const content = useRef<HTMLDivElement>(null)
+  const { height, setHeight } = useStore()
+
+  useEffect(() => {
+    const target = content?.current
+    if (target) {
+      target.onwheel = (e: WheelEvent) => {
+        if (e.ctrlKey) {
+          setHeight(Math.min(400, Math.max(40, height - e.deltaY / 10)))
+          e.preventDefault()
+        }
+      }
+    }
+  }, [content, height, setHeight])
+
   return (
     <div className={styles.container}>
       <div className={styles.header}>
         <TimelineHeader />
       </div>
-      <div className={styles.content}>
+      <div ref={content} className={styles.content}>
         <TimelineContent />
       </div>
     </div>
